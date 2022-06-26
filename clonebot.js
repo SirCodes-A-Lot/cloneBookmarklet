84376879a17104d6c2377930613868ffc2bb8559
SirCodes-A-Lot <aaronaaron@hotmail.co.uk>
2022-06-26 14:00:56 +0100

    added scroll to when and where

--- clonebot.js
+++ clonebot.js
@@ -1,9 +1,23 @@
 javascript: (() => {
     const CLONE_OF = "Clone of ";
     const TITLE_ID = "edit-title-0-value";
-    var titleBox = document.getElementById(TITLE_ID);
-    var titleText = titleBox.value;
-    if (titleText.indexOf(CLONE_OF) != -1) {
-        titleBox.value = titleBox.value = titleText.substring('Clone of '.length);
-    }
+
+    const WHEN_AND_WHERE_ID = "edit-group-when-and-where";
+
+    function removeCloneOf() { 
+        const titleBox = document.getElementById(TITLE_ID);
+        const titleText = titleBox.value;
+        if (titleText.indexOf(CLONE_OF) != -1) {
+            titleBox.value = titleBox.value = titleText.substring('Clone of '.length);
+        }
+    };
+
+    function scrollToWhen() {
+        document.getElementById(WHEN_AND_WHERE_ID).scrollIntoView({
+            behavior: 'smooth'
+        });
+    };
+
+    removeCloneOf();
+    scrollToWhen();
   })();
