1035d05e9112d87b6c7f5e81d70cc7acffdd2ac9
SirCodes-A-Lot <aaronaaron@hotmail.co.uk>
2022-06-26 18:19:04 +0100

    made dates update to new date and scroll to leader section

--- clonebot.js
+++ clonebot.js
@@ -2,7 +2,14 @@ javascript: (() => {
     const CLONE_OF = "Clone of ";
     const TITLE_ID = "edit-title-0-value";
 
-    const WHEN_AND_WHERE_ID = "edit-group-when-and-where";
+    const WHEN_AND_WHERE_SECTION_ID = "edit-group-when-and-where";
+
+    const LEADER_SECTION_ID = "edit-group-leader-information";
+
+    const startDateElement = document.getElementById("edit-field-event-start-date-0-value-date");
+    const endDateElement =  document.getElementById("edit-field-event-end-date-0-value-date");
+    const cutOffDateElement =  document.getElementById("edit-field-event-purchase-cut-off-0-value-date");
+
 
     function removeCloneOf() { 
         const titleBox = document.getElementById(TITLE_ID);
@@ -13,11 +20,28 @@ javascript: (() => {
     };
 
     function scrollToWhen() {
-        document.getElementById(WHEN_AND_WHERE_ID).scrollIntoView({
+        document.getElementById(WHEN_AND_WHERE_SECTION_ID).scrollIntoView({
             behavior: 'smooth'
         });
     };
 
-    removeCloneOf();
-    scrollToWhen();
+    function makeAllDatesStartDate(){
+        const newDate = startDateElement.value;
+        endDateElement.value = newDate;
+        cutOffDateElement.value = newDate;
+    };
+
+    function scrollToLeader() {
+        document.getElementById(LEADER_SECTION_ID).scrollIntoView({
+            behavior: 'smooth'
+        });
+    };
+
+    if (startDateElement.value == cutOffDateElement.value) {
+        removeCloneOf();
+        scrollToWhen();
+    } else {
+        makeAllDatesStartDate();
+        scrollToLeader();
+    };
   })();
